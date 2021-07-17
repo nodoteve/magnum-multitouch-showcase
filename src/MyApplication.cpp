b020765bc4bfb700d0b5bc5cf6dc6ebcd79ca6a6
nodoteve <nodoteve@yandex.com>
2021-07-18 00:11:43 +0300

    mouseMoveEvent works now

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -27,7 +27,7 @@ public:
 
 private:
     ImGuiIntegration::Context imgui{NoCreate};
-    std::map<int,std::pair<int,int>> data;
+    std::map<int, std::pair<int, int>> data;
 };
 
 MyApplication::MyApplication(const Arguments &arguments) : Platform::Application{arguments}
@@ -35,9 +35,19 @@ MyApplication::MyApplication(const Arguments &arguments) : Platform::Application
     imgui = ImGuiIntegration::Context(Vector2{windowSize()} / dpiScaling(),
                                       windowSize(), framebufferSize());
 
+    ImGui::GetIO().FontGlobalScale=2.f;
+
+    // ImGui::GetIO().Fonts->Clear();
+    // ImGui::GetIO().Fonts->AddFontFromFileTTF("font/SourceSansPro-Regular.ttf", 60.0f);
+
+    // imgui.relayout(windowSize());
+
+
     /* Set up proper blending to be used by ImGui. There's a great chance
        you'll need this exact behavior for the rest of your scene. If not, set
        this only for the drawFrame() call. */
+    GL::Renderer::enable(GL::Renderer::Feature::Blending);
+    GL::Renderer::enable(GL::Renderer::Feature::ScissorTest);
     GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add,
                                    GL::Renderer::BlendEquation::Add);
     GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::SourceAlpha,
@@ -59,45 +69,32 @@ void MyApplication::drawEvent()
        Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appear in
        a window called "Debug" automatically */
     {
-        ImGui::Begin("Hello, world!");
-        ImGui::SetWindowPos(ImVec2(650, 350));
-        ImGui::Text("Hello, world!");
-        ImGui::End();
-
+        ImGui::Begin("fps");
+        ImGui::SetWindowPos(ImVec2(10, 10));
+        ImGui::SetWindowSize(ImVec2(400, 100));
+        
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-            1000.0/Double(ImGui::GetIO().Framerate), Double(ImGui::GetIO().Framerate));
-
-
-        for(auto&e:data){
-        auto& a=e.first;
-        auto& b=e.second;
-        auto s=std::to_string(a).data();
-        ImGui::Begin(s);
-        ImGui::SetWindowPos(ImVec2(b.first,b.second));
-        ImGui::SetWindowSize(ImVec2(100,100));
-        ImGui::Text(s);
+                    1000.0 / Double(ImGui::GetIO().Framerate), Double(ImGui::GetIO().Framerate));
         ImGui::End();
+
+
+        for (auto &e : data)
+        {
+            auto &a = e.first;
+            auto &b = e.second;
+            auto s = std::to_string(a).data();
+            ImGui::Begin(s);
+            ImGui::SetWindowPos(ImVec2(b.first, b.second));
+            ImGui::SetWindowSize(ImVec2(100, 100));
+            ImGui::Text(s);
+            ImGui::End();
         }
     }
 
     /* Update application cursor */
     // imgui.updateApplicationCursor(*this);
 
-    /* Set appropriate states. If you only draw ImGui, it is sufficient to
-       just enable blending and scissor test in the constructor. */
-    GL::Renderer::enable(GL::Renderer::Feature::Blending);
-    GL::Renderer::enable(GL::Renderer::Feature::ScissorTest);
-    GL::Renderer::disable(GL::Renderer::Feature::FaceCulling);
-    GL::Renderer::disable(GL::Renderer::Feature::DepthTest);
-
     imgui.drawFrame();
-
-    /* Reset state. Only needed if you want to draw something else with
-       different state after. */
-    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
-    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
-    GL::Renderer::disable(GL::Renderer::Feature::ScissorTest);
-    GL::Renderer::disable(GL::Renderer::Feature::Blending);
 
     swapBuffers();
     redraw();
@@ -105,47 +102,46 @@ void MyApplication::drawEvent()
 
 void MyApplication::mousePressEvent(MouseEvent &event)
 {
-    #ifdef CORRADE_TARGET_ANDROID
-    data[event.pointerId()]={event.position().x(),event.position().y()};
-    Debug{}<<"a"<<event.pointerId()<<" "<<event.pointerIndex();
+#ifdef CORRADE_TARGET_ANDROID
+    data[event.pointerId()] = {event.position().x(), event.position().y()};
+    // Debug{} << "a" << event.pointerId() << event.pointerIndex()<<data[event.pointerId()];
 
-    #else
-    data[1]={event.position().x(),event.position().y()};
-    #endif
-
+#else
+    data[1] = {event.position().x(), event.position().y()};
+#endif
 
     if (imgui.handleMousePressEvent(event))
         return;
-
 }
 
 void MyApplication::mouseReleaseEvent(MouseEvent &event)
 {
-    #ifdef CORRADE_TARGET_ANDROID
-    data.erase(event.pointerId());
-    Debug{}<<"r"<<event.pointerId()<<" "<<event.pointerIndex();
+#ifdef CORRADE_TARGET_ANDROID
+    data[event.pointerId()]={0,0};
+    // data.erase(event.pointerId());
+    // Debug{} << "r" << event.pointerId() << event.pointerIndex();
 
-    #else
+#else
     data[1]={0,0};
-    #endif
+    // data.erase(1);
+#endif
 
     if (imgui.handleMouseReleaseEvent(event))
         return;
-
 }
 
 void MyApplication::mouseMoveEvent(MouseMoveEvent &event)
 {
-    #ifdef CORRADE_TARGET_ANDROID
-    //    data[event.pointerId()]={event.position().x(),event.position().y()};
-    // Debug{}<<"a"<<event.pointerId()<<" "<<event.pointerIndex();
-    #else
-    data[1]={event.position().x(),event.position().y()};
-    #endif
+#ifdef CORRADE_TARGET_ANDROID
+   data[event.pointerId()]={event.position().x(),event.position().y()};
+//    Debug{}<<event.position();
+// Debug{}<<"m"<<event.pointerId()<<event.pointerIndex()<<  event.pointerCount();
+#else
+    // data[1] = {event.position().x(), event.position().y()};
+#endif
 
     if (imgui.handleMouseMoveEvent(event))
         return;
-
 }
 
 MAGNUM_APPLICATION_MAIN(MyApplication)
